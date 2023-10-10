26d5878c67a4265a07f6320be96596056d5d08e7
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 20:13:57 +0530

    Added ViewUpdate to SwitchViews

--- react-sat/src/App.tsx
+++ react-sat/src/App.tsx
@@ -4,11 +4,13 @@ import Header from "./components/Header";
 import ViewTable from "./components/ViewTable";
 import ViewRank from "./components/ViewRank";
 import ViewDelete from "./components/ViewDelete";
+import ViewUpdate from "./components/ViewUpdate";
 
 function SwitchViews({ mode, searchTerm }: { mode: string, searchTerm: string }) {
   switch (mode) {
     case 'view': return <ViewTable />;
     case 'get': return <ViewRank term={searchTerm} />;
+    case 'update': return <ViewUpdate searchTerm={searchTerm} />
     case 'delete': return <ViewDelete searchTerm={searchTerm} />
     default: return <h1 className='m-2 text-lg'>Unknown view: {mode}</h1>;
   }
