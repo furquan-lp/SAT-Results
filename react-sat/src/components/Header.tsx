d425566b0a30d80f01de14bfffff052fffe7ff11
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 19:28:58 +0530

    Show the search input and button on get, update and delete, and set the text on the button to 'Delete' on the delete mode

--- react-sat/src/components/Header.tsx
+++ react-sat/src/components/Header.tsx
@@ -6,12 +6,13 @@ export default function Header({ setMode, setSearch, mode }: { setMode: Function
     <header className='flex justify-between bg-slate-600 text-white p-2 m-2'>
       <span className='flex items-center justify-center gap-3'>
         <span className='md:text-2xl font-mono select-none'>SAT_Result</span>
-        {mode === 'get' ? <span className='flex gap-1 items-center'>
-          <input type='text' placeholder='Type a name to search...' className='p-1 my-0.5 text-slate-700'
-            onChange={(e) => searchQuery.current = e.target.value} />
-          <button className='bg-slate-500 hover:bg-slate-400 px-3 h-8 text-lg'
-            onClick={() => setSearch(searchQuery.current)}>Search</button>
-        </span> : null}
+        {mode === 'get' || mode === 'update' || mode === 'delete'
+          ? <span className='flex gap-1 items-center'>
+            <input type='text' placeholder='Type a name to search...' className='p-1 my-0.5 text-slate-700'
+              onChange={(e) => searchQuery.current = e.target.value} />
+            <button className='bg-slate-500 hover:bg-slate-400 px-3 h-8 text-lg'
+              onClick={() => setSearch(searchQuery.current)}>{mode === 'delete' ? <>Delete</> : <>Search</>}</button>
+          </span> : null}
 
       </span>
       <select className='bg-slate-500 p-1' onChange={(e) => setMode(e.target.value)}>
