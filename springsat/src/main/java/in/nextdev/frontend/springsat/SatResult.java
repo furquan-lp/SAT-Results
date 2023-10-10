1dc3f09dee2bb1fb708dc82da286b68e6a118191
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 16:57:11 +0530

    Ensure the name is a Unique Key

--- springsat/src/main/java/in/nextdev/frontend/springsat/SatResult.java
+++ springsat/src/main/java/in/nextdev/frontend/springsat/SatResult.java
@@ -1,5 +1,6 @@
 package in.nextdev.frontend.springsat;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,7 +10,9 @@ import jakarta.persistence.Table;
 @Table(name = "SATRESULT")
 public class SatResult {
     private @Id @GeneratedValue Long id;
-    private String name, address, city, country;
+    @Column(unique=true)
+    private String name;
+    private String address, city, country;
     private int score;
     private long pincode;
 
