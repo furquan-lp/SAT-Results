3455d82c0d5d92dbe7d74050b9f24c00551b7e1f
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 18:27:59 +0530

    Added input search field, search button and the ref and state prop associated with it

--- react-sat/src/components/Header.tsx
+++ react-sat/src/components/Header.tsx
@@ -1,7 +1,18 @@
-export default function Header({ setMode }: { setMode: Function }) {
+import { useRef } from "react";
+
+export default function Header({ setMode, setSearch, mode }: { setMode: Function, setSearch: Function, mode: string }) {
+  let searchQuery = useRef('');
   return (
     <header className='flex justify-between bg-slate-600 text-white p-2 m-2'>
-      <span className='md:text-2xl font-mono select-none'>SAT_Result</span>
+      <span className='flex items-center justify-center gap-3'>
+        <span className='md:text-2xl font-mono select-none'>SAT_Result</span>
+        <span className='flex gap-1 items-center'>
+          <input type='text' placeholder='Type a name to search...' className='p-1 my-0.5 text-slate-700'
+            onChange={(e) => searchQuery.current = e.target.value} />
+          <button className='bg-slate-500 hover:bg-slate-400 px-3 h-8 text-lg'
+            onClick={() => setSearch(searchQuery.current)}>Search</button>
+        </span>
+      </span>
       <select className='bg-slate-500 p-1' onChange={(e) => setMode(e.target.value)}>
         <option value='view'>View all data</option>
         <option value='insert'>Insert data</option>
