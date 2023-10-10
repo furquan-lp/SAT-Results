86064dc41ac04103a5349e9f3e4f8e4e16849579
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 13:34:00 +0530

    Added files from the Spring project

---
+++ springsat/src/main/java/in/nextdev/frontend/springsat/SpringSatApplication.java
@@ -0,0 +1,13 @@
+package in.nextdev.frontend.springsat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSatApplication.class, args);
+	}
+
+}
---
+++ springsat/src/test/java/in/nextdev/frontend/springsat/SpringSatApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nextdev.frontend.springsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
