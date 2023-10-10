7891808f586f3dc2a250317269b0ca886d62c506
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 19:43:30 +0530

    Added a ViewDelete component to handle running deletion requests

---
+++ react-sat/src/components/ViewDelete.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function ViewDelete({ searchTerm }: { searchTerm: string }) {
+  const [deleted, setDeleted] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let tx = await fetch(`http://localhost:8080/results/${searchTerm}`, {
+        method: "DELETE"
+      });
+      if (tx.status === 200) {
+        setDeleted(true);
+      } else {
+        setDeleted(false);
+      }
+    })();
+  }, [searchTerm]);
+
+  if (searchTerm.length < 1) return <></>;
+
+  if (deleted === null) {
+    return (
+      <div className='mx-2 my-6 text-slate-600'>Deleting.</div>
+    );
+  } else if (deleted) {
+    return (
+      <div className='mx-2 my-6 text-slate-700'>Successfully Deleted '{searchTerm}'.</div>
+    );
+  } else {
+    return (
+      <div className='mx-2 my-6 text-slate-500'>Not found.</div>
+    );
+  }
+}
