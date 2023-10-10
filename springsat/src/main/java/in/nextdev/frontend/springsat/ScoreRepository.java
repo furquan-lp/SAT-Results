e491e4934636c3f4a1299fd871ba44c39ae19780
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 16:53:37 +0530

    Added findByName method with SQL query to find a value by name in the table

--- springsat/src/main/java/in/nextdev/frontend/springsat/ScoreRepository.java
+++ springsat/src/main/java/in/nextdev/frontend/springsat/ScoreRepository.java
@@ -1,8 +1,11 @@
 package in.nextdev.frontend.springsat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 interface ScoreRepository extends JpaRepository<SatResult, Long>{
-    
+
+    @Query(value = "SELECT * FROM satresult WHERE name=?", nativeQuery = true)
+    SatResult findByName(String name);
 }
