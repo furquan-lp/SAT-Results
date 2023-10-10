473c419cc95ab8d67b41d1523694b8145b4239fb
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 19:45:48 +0530

    Clear the search state on change of the select menu and removed underscore from the logo text

--- react-sat/src/components/Header.tsx
+++ react-sat/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header({ setMode, setSearch, mode }: { setMode: Function
   return (
     <header className='flex justify-between bg-slate-600 text-white p-2 m-2'>
       <span className='flex items-center justify-center gap-3'>
-        <span className='md:text-2xl font-mono select-none'>SAT_Result</span>
+        <span className='md:text-2xl font-mono select-none'>SAT Result</span>
         {mode === 'get' || mode === 'update' || mode === 'delete'
           ? <span className='flex gap-1 items-center'>
             <input type='text' placeholder='Type a name to search...' className='p-1 my-0.5 text-slate-700'
@@ -15,7 +15,10 @@ export default function Header({ setMode, setSearch, mode }: { setMode: Function
           </span> : null}
 
       </span>
-      <select className='bg-slate-500 p-1' onChange={(e) => setMode(e.target.value)}>
+      <select className='bg-slate-500 p-1' onChange={(e) => {
+        setMode(e.target.value);
+        setSearch('');
+      }}>
         <option value='view'>View all data</option>
         <option value='insert'>Insert data</option>
         <option value='get'>Get rank</option>
