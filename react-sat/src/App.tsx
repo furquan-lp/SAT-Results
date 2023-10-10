4a9e7756b3141cf2a34a644003212a53fa0fc2fc
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 18:50:07 +0530

    Added the ViewRank component to show information about the searched candidate

--- react-sat/src/App.tsx
+++ react-sat/src/App.tsx
@@ -132,10 +132,49 @@ function ViewTable() {
   );
 }
 
-function SwitchViews({ mode }: { mode: string }) {
+function ViewRank({ term }: { term: string }) {
+  if (term === null || term.length < 1) {
+    return <></>;
+  } else {
+    const [currentResult, setCurrentResult] = useState<{
+      id: number, name: string, address: string, city: string, country: string,
+      pincode: number, score: number, passed: boolean
+    } | null>(null);
+
+    useEffect(() => {
+      (async () => {
+        let tx = await fetch(`http://localhost:8080/results/${term}`);
+        if (tx.status === 200) {
+          let data = await tx.json();
+          setCurrentResult(data);
+        } else {
+          setCurrentResult(null);
+        }
+      })();
+    }, [term]);
+
+    if (currentResult === null) {
+      return <div className='mx-2 my-6 text-slate-400'>No results found.</div>;
+    } else {
+      return (
+        <article className='flex flex-col mx-2 bg-slate-200 p-1 rounded-md text-lg'>
+          <span>Name: {currentResult.name}</span>
+          <span>Address: {currentResult.address}</span>
+          <span>City: {currentResult.city}</span>
+          <span>Country: {currentResult.country}</span>
+          <span>Pincode: {currentResult.pincode}</span>
+          <span>Score: {currentResult.score}%</span>
+          <span>Passed: {currentResult.passed ? 'Yes' : 'No'}</span>
+        </article>
+      );
+    }
+  }
+}
+
+function SwitchViews({ mode, searchTerm }: { mode: string, searchTerm: string }) {
   switch (mode) {
     case 'view': return <ViewTable />;
-    case 'get': return <></>;
+    case 'get': return <ViewRank term={searchTerm} />;
     default: return <h1 className='m-2 text-lg'>Unknown view: {mode}</h1>;
   }
 }
@@ -149,7 +188,7 @@ function App() {
   return (
     <main>
       <Header setMode={setViewMode} mode={viewMode} setSearch={setSearchTerm} />
-      <SwitchViews mode={viewMode} />
+      <SwitchViews mode={viewMode} searchTerm={searchTerm} />
     </main>
   );
 }
