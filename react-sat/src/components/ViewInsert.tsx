22fb7c826eb2b6e71191070116c50b4d06fd2094
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 20:36:05 +0530

    Added state and ref for the form SAT object

--- react-sat/src/components/ViewInsert.tsx
+++ react-sat/src/components/ViewInsert.tsx
@@ -1,14 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function ViewInsert() {
   let currentSATResult = useRef<{
     name: string; address: string; city: string; country: string; pincode: number; score: number;
-  }>({
-    name: '', address: '', city: '', country: '', pincode: -1, score: -1
-  });
+  } | null>(null);
+  const [SATResult, setSATResult] = useState<{
+    name: string, address: string, city: string, country: string, pincode: number, score: number
+  } | null>(null);
 
   return (
-    <article className='flex flex-col mx-2 my-6 md:mx-96 gap-y-2 p-4 bg-slate-100 rounded shadow items-center text-lg'>
+    <article className='flex flex-col mx-2 my-6 md:mx-60 xl:mx-80 2xl:mx-96 gap-y-2 p-4 bg-slate-100 rounded shadow items-center text-lg'>
       <h1 className='text-xl font-bold mb-2'>Enter Candidate Details</h1>
       <span className='flex gap-x-5 w-full items-center'>Name:<input className='p-1 w-full border' /></span>
       <span className='flex gap-x-1 w-full'>Address:<textarea className='p-1 w-full h-28 border' /></span>
