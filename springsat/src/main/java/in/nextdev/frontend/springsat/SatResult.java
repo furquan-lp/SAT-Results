75dc196d2666016a5b055dbb39e4e082fdf26e19
Syed F Ahmad <furquan@protonmail.com>
2023-10-10 16:08:29 +0530

    Added the class for the SAT result object

---
+++ springsat/src/main/java/in/nextdev/frontend/springsat/SatResult.java
@@ -0,0 +1,76 @@
+package in.nextdev.frontend.springsat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class SatResult {
+    private @Id @GeneratedValue Long id;
+    private String name, address, city, country;
+    private int score;
+    private long pincode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public long getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(long pincode) {
+        this.pincode = pincode;
+    }
+
+    public boolean passed() {
+        return score > 30;
+    }
+
+    public SatResult() {}
+
+    public SatResult(String name, String address, String city, String country, int score, long pincode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.score = score;
+        this.pincode = pincode;
+    }
+}
